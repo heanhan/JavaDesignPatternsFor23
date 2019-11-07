3bc9dfabd39732c5cf04cb2ed91690a1557456b7
heanhan <1763124707@qq.com>
2019-11-07 19:14:20 +0800

    java的23中设计模式的学习笔记：单例模式的学习，八种单例模式之一的饿汉模式  的理论学习，外加代码的实现。

--- java-singleton-pattern/src/com/zhaojh/method/SingletonOne.java
+++ java-singleton-pattern/src/com/zhaojh/method/SingletonOne.java
@@ -3,8 +3,38 @@ package com.zhaojh.method;
 /**
  * @author : zhaojh
  * @date : 2019-11-07
- * @function :
- */
+ * @function :单例模式  饿汉式   线程安全   本质使用静态常量
+ *  优点：写法简单明了，就是在类转载的时候就完成实例化，避免了线程的同步问题
+ *  缺点：在类加载的时候就完成实例化，没有达到Lazy Loading 的效果，如果从始至终未使用过这个实例，则会造成内存的浪费。
+ *
+ * */
+
 
 public class SingletonOne {
+
+    /**
+     * 1、提供静态常量
+     */
+
+    private final static SingletonOne singletonOne= new SingletonOne();
+
+
+    /**
+     * 2、构造方法的私有化
+     */
+    private SingletonOne(){}
+
+    /**
+     *
+     * 3、对外提供获取实例的方法
+     */
+    public static SingletonOne getInstance(){
+        return singletonOne;
+    }
+
+
+    public static void main(String[] args) {
+        SingletonOne instance = SingletonOne.getInstance();
+        System.out.println("获取的单例的实例为："+instance);
+    }
 }
